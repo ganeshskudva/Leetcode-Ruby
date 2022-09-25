36ce3c375506a9d6ad6d0a306f07516799c011fd
Ganesh S Kudva <ganeshskudva@gmail.com>
2022-09-24 17:01:37 -0700

    Update L509.rb

--- Easy/L509.rb
+++ Easy/L509.rb
@@ -1,16 +1,13 @@
 # @param {Integer} n
 # @return {Integer}
 def fib(n)
-  mp = {}
-  mp[0] = 0
-  mp[1] = 1
-  mp[2] = 1
+  @mp = { 0 => 0, 1 => 1, 2 => 1}
 
-  solve(mp, n)
+  solve(n)
 end
 
-def solve(mp, n)
-  return mp[n] if mp.key?(n)
+def solve(n)
+  return @mp[n] if @mp.include?(n)
 
-  mp[n] = solve(mp, n - 1) + solve(mp, n - 2)
+  @mp[n] = solve(n - 1) + solve(n - 2)
 end
